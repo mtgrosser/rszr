9021db1f33c35da2e152b317b67474de68a4e3f7
Matthias Grosser <mtgrosser@gmx.net>
2019-03-07 16:21:49 +0100

    Typo fix

--- ext/rszr/extconf.rb
+++ ext/rszr/extconf.rb
@@ -30,8 +30,8 @@ unless find_header('Imlib2.h')
   abort 'imlib2 development headers are missing'
 end
 
-unless find_library('imlib2', 'imlib_set_cache_size')
-  abort 'imlib2 is missing'
+unless find_library('Imlib2', 'imlib_set_cache_size')
+  abort 'Imlib2 is missing'
 end
 
 create_makefile 'rszr'
